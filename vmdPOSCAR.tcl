de8967f5651a9f628d70f7575d792531bab76d61
Kyle Mills <kyle.mills@uoit.net>
2015-06-02 16:20:31 -0400

    script to open a POSCAR file in vmd

---
+++ vmdPOSCAR.tcl
@@ -0,0 +1,6 @@
+# VMD for LINUXAMD64, version 1.9.2 (December 29, 2014)
+# Log file '/home/kmills/Dropbox/SCRIPTS/vmdPOSCAR.tcl', created by user kmills
+mol new {POSCAR} type {POSCAR} first 0 last -1 step 1 waitfor 1
+mol modstyle 0 0 VDW 0.400000 12.000000
+# VMD for LINUXAMD64, version 1.9.2 (December 29, 2014)
+# end of log file.

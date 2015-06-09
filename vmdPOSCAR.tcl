84abbacde204f14e3eb0e9fcaa6d266998166837
Kyle Mills <kyle.mills@uoit.net>
2015-06-09 10:15:25 -0400

    vmdPOSCAR Al now silver, not pink

--- vmdPOSCAR.tcl
+++ vmdPOSCAR.tcl
@@ -4,3 +4,8 @@ mol new {POSCAR} type {POSCAR} first 0 last -1 step 1 waitfor 1
 mol modstyle 0 0 VDW 0.400000 12.000000
 # VMD for LINUXAMD64, version 1.9.2 (December 29, 2014)
 # end of log file.
+
+
+color Name A silver
+
+

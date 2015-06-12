2ca52944b53a44298181cfd3509f2a7310dc0882
Kyle Mills <kyle.mills@uoit.net>
2015-06-12 09:58:22 -0400

    added cluster to sync

--- sync_to_clusters.sh
+++ sync_to_clusters.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-destinations="orca:/home/kmills/work/bin bgq:/home/i/itamblyn/kmills/bin"
+destinations="orca:/home/kmills/work/bin bgq:/home/i/itamblyn/kmills/bin orcinus:/home/kmills/bin"
 
 for dest in $destinations; do
 

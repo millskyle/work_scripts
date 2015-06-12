e67e19b601eba6f9012f0d6b2377e0904d96a951
Kyle Mills <kyle.mills@uoit.net>
2015-06-12 13:49:48 -0400

    wrapper for bgq qsub files

--- cluster_aliases_a.sh
+++ cluster_aliases_a.sh
@@ -4,4 +4,29 @@ alias cd.='cd .. # '  # I often type 'cd. .' instead of 'cd ..'. This will fix t
 
 
 
+function qsubwrap() {
+#!/bin/bash
+
+inputFile="$1"
+if [[ "x$(   hostname | grep -o '[a-zA-Z]*-' )" == "xbgqdev-" ]]; then
+   if [[ ! "x$1" == "x" ]]; then
+      if [[ -a "$1" ]]; then
+         sed "s|!!!basename!!!|$(basename $PWD)|g" $1 > "${1}.tmp"
+         llsubmit "${1}.tmp"
+         rm "${1}.tmp" 2>/dev/null
+      else
+         echo "File $1 doesn't exist!"
+      fi
+   else
+      echo "You must specify the submit script!" 
+   fi
+else
+  echo "This script can only be run on Blue Gene Q (bgqdev)"
+fi
+
+}
+
+
+
+
 

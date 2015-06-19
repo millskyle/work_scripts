ea988181675df370e3fd82732eefa3b6a9078f2d
Kyle Mills <kyle.mills@uoit.net>
2015-06-19 08:13:59 -0400

    directory looping function/alias created

--- cluster_aliases_a.sh
+++ cluster_aliases_a.sh
@@ -3,6 +3,20 @@
 alias cd.='cd .. # '  # I often type 'cd. .' instead of 'cd ..'. This will fix this
 
 
+#Function and alias for looping over all directories within current directory.
+#The variable $d can be used to reference the current subdirectory in the loop.
+#
+#   Usage:     foralldirs <do something>; done
+#
+#   Example:   foralldirs cd $d; touch file.txt; cd ..; done
+#      will create an empty file called file.txt in every subdirectory.
+function alldirs () { 
+   \ls -d */ | sed 's|\/||g'
+}
+alias foralldirs='for d in $(alldirs); do'
+
+
+
 
 function qsubwrap() {
 #!/bin/bash

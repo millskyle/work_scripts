9eae1e658cd099fb0e1463b2926bd19ab56a9bc9
Kyle Mills <kyle.mills@uoit.net>
2015-06-19 07:49:21 -0400

    new cluster support added to qsubwrap() function

--- cluster_aliases_a.sh
+++ cluster_aliases_a.sh
@@ -7,7 +7,8 @@ alias cd.='cd .. # '  # I often type 'cd. .' instead of 'cd ..'. This will fix t
 function qsubwrap() {
 #!/bin/bash
 
-inputFile="$1"
+
+
 if [[ "x$(   hostname | grep -o '[a-zA-Z]*-' )" == "xbgqdev-" ]]; then
    if [[ ! "x$1" == "x" ]]; then
       if [[ -a "$1" ]]; then
@@ -20,8 +21,23 @@ if [[ "x$(   hostname | grep -o '[a-zA-Z]*-' )" == "xbgqdev-" ]]; then
    else
       echo "You must specify the submit script!" 
    fi
+
+
+elif [[ "x$( hostname )" == "xbugaboo.westgrid.ca" ]]; then
+   if [[ ! "x$1" == "x" ]]; then
+      if [[ -a "$1" ]]; then
+         sed "s|!!!basename!!!|$(basename $PWD)|g" $1 > "${1}.tmp"
+         qsub "${1}.tmp"
+         rm "${1}.tmp" 2>/dev/null
+      else
+         echo "File $1 doesn't exist!"
+      fi
+   else
+      echo "You must specify the submit script!" 
+   fi
+
 else
-  echo "This script can only be run on Blue Gene Q (bgqdev)"
+  echo "This script can only be run on specific clusters.  The current hostname  [$(hostname)] is not compatible.  add it to \$WORKSCRIPTS/cluster_aliases_a.sh in the qsubwrap() function."
 fi
 
 }
